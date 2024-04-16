ffd0cddad1f64807735e2d86e97e3c5a18d5296d
JGriffin <johngrif.dev@gmail.com>
2024-04-15 22:22:19 -0700

    Fix Chart3DTests build

--- Packages/Charts3D/Tests/Charts3DTests/Chart3DTests.swift
+++ Packages/Charts3D/Tests/Charts3DTests/Chart3DTests.swift
@@ -7,18 +7,18 @@ import Spatial
 import XCTest
 
 final class BoundsTests: XCTestCase {
-    let oneTwoThree = Point3D([1.0, 2.0, 3.0])
-    let tenTenTen = Point3D([10.0, 10, 10])
-    let elevenTwelveThirteen = Point3D([11.0, 12, 13])
+    let oneTwoThree = SIMD3<Float>([1.0, 2.0, 3.0])
+    let tenTenTen = SIMD3<Float>([10.0, 10, 10])
+    let elevenTwelveThirteen = SIMD3<Float>([11.0, 12, 13])
 
     func testBoxBounds() {
         let box = Box3D(
             size: .init(oneTwoThree),
-            position: tenTenTen
+            position: Point3D(tenTenTen)
         )
-        XCTAssertEqual(box.bounds?.size, Point(oneTwoThree).size)
-        XCTAssertEqual(box.bounds?.min, Point3D(oneTwoThree - 0.5))
-        XCTAssertEqual(box.bounds?.max, Point3D(oneTwoThree + 0.5))
+        XCTAssertEqual(box.bounds?.size, Size3D(oneTwoThree))
+        XCTAssertEqual(box.bounds?.min, Point3D((oneTwoThree - 0.5)))
+        XCTAssertEqual(box.bounds?.max, Point3D((oneTwoThree + 0.5)))
         XCTAssertEqual(box.containedBounds?.min, Point3D(tenTenTen))
         XCTAssertEqual(box.containedBounds?.max, Point3D([11.0, 12, 13]))
     }
